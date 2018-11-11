9ba1ef8ba42c24e27c5b3c0704143edb8f8c004b
James Stewart <jstew655@mtroyal.ca>
2018-11-11 21:42:05 +0000

    Commit before work on structure

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Home Page</title>
+</head>
+<bod>
+<header>
+    <img src="" id="logo"/>
+    <h1 id="websiteTitle">Title of website</h1>
+    <h2 id="websiteSubtitle">subtittle</h2>
+    <span id="headerLinks">LINK TO PAGES HERE</span>
+</header>
+<div id="galleryList">
+    
+</div>
+<div id="artistList">
+    
+</div>
+<div id="genreList">
+    
+</div>
+</bod>
+</html>
