3ee6f0c4efd17b731558a96955bcc730e12fecf9
James Stewart <jstew655@mtroyal.ca>
2018-11-13 21:13:38 +0000

    populated all windows on start screen

--- index.php
+++ index.php
@@ -39,9 +39,9 @@ require_once 'config.inc.php';
 </table>    
 </div>
 
-    <div id="galleryList">
-               <?php  
-                   // output all the retrieved galleries (hint: set value attribute of <option> to the GalleryID field)
+<div id="galleryList">
+<?php  
+// output all the retrieved galleries (hint: set value attribute of <option> to the GalleryID field)
 
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME .";charset=utf8mb4;");
 try {
@@ -55,7 +55,7 @@ try {
  
  while ($row = $result->fetch()) {
    
-  echo "<li><A href='". $row['GalleryID'] ."'>". $row['GalleryName']  . "</a></li>";
+  echo "<li><A href='singleGalleryPage.php?GalleryID=". $row['GalleryID'] ."'>". $row['GalleryName']  . "</a></li>";
 
  }
 //</ul>
@@ -67,11 +67,58 @@ catch (PDOException $e) {
   ?>
   
     </div>
-    <div id="artistList">
-artistList
+    <div id="artistList"><!-- max-height: 1%; -->
+<?php  
+// output all the retrieved galleries (hint: set value attribute of <option> to the GalleryID field)
+
+define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME .";charset=utf8mb4;");
+try {
+ $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ $sql = "SELECT Artists.ArtistID,Firstname, LastName,ImageFileName  
+         FROM Artists,Paintings 
+         WHERE Artists.ArtistID =Paintings.ArtistID
+         Group by Artists.ArtistID";
+ $result = $pdo->query($sql);
+
+
+ while ($row = $result->fetch()) {
+   
+  echo " &nbsp <A href='singleArtistPage.php?ArtistID=". $row['ArtistID'] ."'>". $row['FirstName'] . $row['LastName'] ." IFM:". $row['ImageFileName'] . "</a> &nbsp ";
+
+ }
+
+}
+catch (PDOException $e) {
+ die( $e->getMessage() );
+}
+  ?>
     </div>
     <div id="genreList">
-genrelist
+<?php  
+// output all the retrieved galleries (hint: set value attribute of <option> to the GalleryID field)
+
+define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME .";charset=utf8mb4;");
+try {
+ $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ $sql = "SELECT * FROM Genres";
+ $result = $pdo->query($sql);
+
+
+ while ($row = $result->fetch()) {
+   
+  echo " &nbsp <A href='singleGenrePage.php?GenreID=". $row['GenreID'] ."'>". $row['GenreName'] . "</a> &nbsp ";
+
+ }
+
+}
+catch (PDOException $e) {
+ die( $e->getMessage() );
+}
+  ?>
     </div>
 </main>
 </body>
